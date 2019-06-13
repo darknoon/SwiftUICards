3d0767f94b8b4cc46715023d96f64c05c189beff
Ollie Wagner <olliewagner@mac.com>
2019-06-13 14:57:50 -0600

    Made list scroll

--- SwiftUICards/Views/ListView.swift
+++ SwiftUICards/Views/ListView.swift
@@ -3,14 +3,18 @@ import SwiftUI
 struct ListView : View {
     var body: some View {
         NavigationView {
-            ScrollView {
-                ForEach(0 ..< 5) { item in
-                    ListCard(title: "Card \(item)")
-                        .relativeWidth(1.0)
-                        .padding([.bottom, .leading, .trailing])
-                }
-                }
-                .navigationBarTitle(Text("Too many cards!"))
+			GeometryReader { geo in
+				ScrollView {
+					ForEach(0 ..< 5) { item in
+						ListCard(title: "Card \(item)")
+							.padding([.bottom, .leading, .trailing])
+							.frame(width: geo.size.width, height: geo.size.width * 1.333)
+							.shadow(color: Color(.sRGBLinear, white: 0, opacity: 0.33), radius: 20, x: 0, y: 10)
+						
+					}
+				}
+				.navigationBarTitle(Text("Too many cards!"))
+			}
         }
     }
 }
